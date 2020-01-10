9718c36f13b67765049a6439f30ee9d1867d1af0
Abhishek5101 <miabhishekkulkarni@gmail.com>
2020-01-10 11:36:58 -0800

    Procfile recreated

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -6,6 +6,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '567fbdf4643402c860b90b823a57df29'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['DATABASE_URL'] = 'postgres://sxeyeqbbivckfk:013bbfe40cc1d4083ed02566545c15925d1eef213a5d3d127688bb4c4a11fd7f@ec2-174-129-254-217.compute-1.amazonaws.com:5432/d124j9q9lhsfbk'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)

8efc42cf50521b761d4fdd88ef1a472d5544d43f
Abhishek5101 <miabhishekkulkarni@gmail.com>
2020-01-10 18:10:19 -0800

    Added comments

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -8,12 +8,18 @@ import json
 
 TMDB_API_KEY = '23efb496361ed8a18ef9bc9238bf5c14'
 
-
+"""
+Home route to index page
+"""
 @app.route('/')
 def home():
 	return render_template('index.html')
 
-
+"""
+Register route takes in both GET and POST requests
+if form in validated, user is added to the DB and redirected to Login page
+if not, they are returned to same route with validation errors
+"""
 @app.route("/register", methods=['GET', 'POST'])
 def register():
 	if current_user.is_authenticated:
@@ -28,7 +34,10 @@ def register():
 		return redirect(url_for('login'))
 	return render_template('register.html', title='Register', form=form)
 
-
+"""
+Login Route authenticates the user and if it fails, returns back to self
+with validation errors
+"""
 @app.route("/login", methods=['GET', 'POST'])
 def login():
 	if current_user.is_authenticated:
@@ -44,19 +53,27 @@ def login():
 			flash('Login Unsuccessful. Please check email and password', 'danger')
 	return render_template('login.html', title='Login', form=form)
 
-
+"""
+Logs the user out
+"""
 @app.route("/logout")
 def logout():
 	logout_user()
 	return redirect(url_for('home'))
 
-
+"""
+This route will be more useful later when creating user profiles
+"""
 @app.route("/account")
 @login_required
 def account():
 	return render_template('account.html', title='Account')
 
-
+"""
+movies route has a default search query of word 'up'
+you can change that by making your own searches though search bar
+as a POST request which will again call the IMDB API
+"""
 @app.route("/movies", methods=['GET', 'POST'])
 def show_movies():
 	if request.method == 'POST':
